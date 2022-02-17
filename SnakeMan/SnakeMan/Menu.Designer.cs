0c7dec61caa4f5745707e8987c75119c2bf82779
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2022-02-17 18:27:18 +0100

    update, added menu demo

---
+++ SnakeMan/SnakeMan/Menu.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SnakeMan
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Easy = new System.Windows.Forms.Button();
+            this.Normal = new System.Windows.Forms.Button();
+            this.Hard = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 93);
+            this.label1.MaximumSize = new System.Drawing.Size(550, 60);
+            this.label1.MinimumSize = new System.Drawing.Size(550, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(550, 60);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SnakeMan";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Easy
+            // 
+            this.Easy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Easy.Location = new System.Drawing.Point(3, 3);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(159, 94);
+            this.Easy.TabIndex = 0;
+            this.Easy.Text = "Easy";
+            this.Easy.UseVisualStyleBackColor = true;
+            this.Easy.Click += new System.EventHandler(this.Easy_Click);
+            // 
+            // Normal
+            // 
+            this.Normal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Normal.Location = new System.Drawing.Point(168, 3);
+            this.Normal.Name = "Normal";
+            this.Normal.Size = new System.Drawing.Size(159, 94);
+            this.Normal.TabIndex = 1;
+            this.Normal.Text = "Normal";
+            this.Normal.UseVisualStyleBackColor = true;
+            this.Normal.Click += new System.EventHandler(this.Normal_Click);
+            // 
+            // Hard
+            // 
+            this.Hard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hard.Location = new System.Drawing.Point(333, 3);
+            this.Hard.Name = "Hard";
+            this.Hard.Size = new System.Drawing.Size(161, 94);
+            this.Hard.TabIndex = 2;
+            this.Hard.Text = "Hard";
+            this.Hard.UseVisualStyleBackColor = true;
+            this.Hard.Click += new System.EventHandler(this.Hard_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.Easy, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Hard, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Normal, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 13);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(497, 100);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(136, 156);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 183);
+            this.panel1.TabIndex = 1;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Easy;
+        private System.Windows.Forms.Button Normal;
+        private System.Windows.Forms.Button Hard;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ SnakeMan/SnakeMan/Menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace SnakeMan
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+            label1.BringToFront();
+
+
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void Easy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Normal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Hard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
